1d1544a93bba35fa702bb8fa3f40ec2a794e05c0
AkankshRakesh <akankshrakesh@gmail.com>
2026-05-07 19:02:00 +0530

    better wait

--- deploy/infra/security.tf
+++ deploy/infra/security.tf
@@ -29,6 +29,13 @@ resource "aws_security_group" "k8s" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
