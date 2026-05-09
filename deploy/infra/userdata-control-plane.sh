a2bf8d1ec01973e1881f9123038f3754ee3ff521
AkankshRakesh <akankshrakesh@gmail.com>
2026-05-09 09:29:14 +0530

    Sudo bash

--- deploy/infra/userdata-control-plane.sh
+++ deploy/infra/userdata-control-plane.sh
@@ -257,7 +257,7 @@ cat <<'EOF' > /home/ubuntu/install-monitoring.sh
 
 set -euxo pipefail
 
-export KUBECONFIG=/etc/kubernetes/admin.conf
+sudo export KUBECONFIG=/etc/kubernetes/admin.conf
 
 helm repo add prometheus-community \
 https://prometheus-community.github.io/helm-charts
